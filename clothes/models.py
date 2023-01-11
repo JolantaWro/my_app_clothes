8da00d585229633833a9894cb178701f3c0ed277
JolantaWro <jolanta.woronowska@gmail.com>
2023-01-11 21:17:23 +0100

    add models

--- clothes/models.py
+++ clothes/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+   name = models.CharField(max_length=255)
+
+   def __str__(self):
+       return self.name
+
+
+TYPE_INSTITUTION = (
+   (1, "Fundacja"),
+   (2, "Organizacja pozarządowa"),
+   (3, "Zbiórka lokalna"),
+)
+
+
+class Institution(models.Model):
+   name = models.CharField(max_length=255)
+   description = models.TextField(null=True, blank=True)
+   type = models.IntegerField(choices=TYPE_INSTITUTION, default=1)
+   categories = models.ManyToManyField(Category)
+
+   def __str__(self):
+       return self.name
+
+
+class Donation(models.Model):
+   quantity = models.IntegerField()
+   categories = models.ManyToManyField(Category)
+   institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+   street_address = models.CharField(max_length=64)
+   city = models.CharField(max_length=64)
+   zip_code = models.CharField(max_length=12)
+   phone_number = models.IntegerField(max_length=15, blank=True, null=True)
+   pick_up_date = models.DateField()
+   pick_up_time = models.TimeField()
+   pick_up_comment = models.TextField(null=True, blank=True)
+   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, default=None)
+
