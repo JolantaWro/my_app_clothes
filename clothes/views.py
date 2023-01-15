f7239c827f95e282cdf6d978aa730c36fa45b3a6
JolantaWro <jolanta.woronowska@gmail.com>
2023-01-15 13:08:35 +0100

    add view register

--- clothes/views.py
+++ clothes/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Permission
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
 from django.views import View
 from clothes.models import Donation
+from clothes.forms import FormRegister
 
 
 class LandingPage(View):
@@ -35,7 +38,44 @@ class LandingPage(View):
 
 class Register(View):
     def get(self, request):
-        return render(request, 'register.html')
+        form = FormRegister()
+        return render(request, 'register.html', {'form': form})
+
+    def post(self, request):
+        form = FormRegister(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data.get('password')
+            password_repeat = form.cleaned_data.get('password_repeat')
+            mail = form.cleaned_data.get('mail')
+            if User.objects.filter(username=mail).exists():
+                raise ValidationError('Użytkownik już istnieje')
+
+            if password != password_repeat:
+                form.add_error(None, 'Wprowadzone różne hasła')
+
+            new_user = User.objects.create_user(username=mail, password=password, email=mail)
+            add_donation = Permission.objects.get(codename='add_donation')
+            view_donation = Permission.objects.get(codename='view_donation')
+            change_donation = Permission.objects.get(codename='change_donation')
+            delete_donation = Permission.objects.get(codename='delete_donation')
+            add_category = Permission.objects.get(codename='add_category')
+            view_category = Permission.objects.get(codename='view_category')
+            change_category = Permission.objects.get(codename='change_category')
+            delete_category = Permission.objects.get(codename='delete_category')
+            add_institution = Permission.objects.get(codename='add_institution')
+            view_institution = Permission.objects.get(codename='view_institution')
+            change_institution = Permission.objects.get(codename='change_institution')
+            delete_institution = Permission.objects.get(codename='delete_institution')
+
+
+            new_user.user_permissions.add(add_donation, view_donation, change_donation, delete_donation, add_category
+                                          , view_category, change_category, delete_category, add_institution
+                                          , view_institution, change_institution, delete_institution)
+
+            new_user.save()
+            return redirect('user_login')
+
+        return redirect('user_login')
 
 
 class Login(View):
